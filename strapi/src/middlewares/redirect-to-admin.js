d2bce952ad58c9872ae6d114b91934b165766f3f
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2022-02-17 15:28:55 +0100

    add redirect from / to /admin

---
+++ strapi/src/middlewares/redirect-to-admin.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * `redirect-to-admin` middleware.
+ */
+
+module.exports = () => {
+  return async (ctx, next) => {
+    if (ctx.path != '/') {
+      await next();
+      return;
+    }
+
+    ctx.response.status = 301;
+    ctx.set({
+      Location: '/admin',
+    });
+  };
+};
