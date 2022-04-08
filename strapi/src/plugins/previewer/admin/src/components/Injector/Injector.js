4c16a3d34c0a4e4991b3dde858f4ef641128148e
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2022-04-08 13:24:15 +0200

    update paddings

--- strapi/src/plugins/previewer/admin/src/components/Injector/Injector.js
+++ strapi/src/plugins/previewer/admin/src/components/Injector/Injector.js
@@ -3,8 +3,7 @@ import React from 'react';
 import Button from '../Button';
 
 const Injector = () => {
-  const { modifiedData, isCreatingEntry, ...rest } = useCMEditViewDataManager();
-  console.log(rest);
+  const { modifiedData, isCreatingEntry } = useCMEditViewDataManager();
 
   const [url, setUrl] = React.useState('');
 
