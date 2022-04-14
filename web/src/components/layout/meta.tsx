e8eb2e3f5374e902a6506b6c4a1aae4ca1483a34
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2022-04-14 21:20:07 +0200

    refactor meta

--- web/src/components/layout/meta.tsx
+++ web/src/components/layout/meta.tsx
@@ -29,29 +29,11 @@ const Meta: React.FC<Props> = ({
         content="E-Sektionen, Nollning, Lunds Universitet, LTH"
       />
 
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#101826" />
-      <meta name="apple-mobile-web-app-title" content="E-Nollning" />
-      <meta name="application-name" content="E-Nollning" />
-      <meta name="msapplication-TileColor" content="#101826" />
-      <meta name="theme-color" content="#ffffff" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color={color} />
+      <meta name="apple-mobile-web-app-title" content={t} />
+      <meta name="application-name" content={t} />
+      <meta name="msapplication-TileColor" content={color} />
+      <meta name="theme-color" content={color} />
 
       <meta property="og:title" content={t} />
       <meta property="og:description" content={description} />
