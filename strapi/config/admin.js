b9bccda13b0169183c16bb8e865dcda5aa0c45d6
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2022-04-20 14:28:32 +0200

    update emailtemplate

--- strapi/config/admin.js
+++ strapi/config/admin.js
@@ -1,6 +1,24 @@
+const emailTemplate = `<p>Hejsan lille <%= user.firstname %>!</p>
+<p>Har du glömt ditt lille lösenord?</p>
+<p>Have no fear, InfU is here!</p>
+
+<p>Klicka på länken nedan så kommer allt lösa sig!</p>
+<p><a href="<%= url %>"><%= url %></a></p>
+
+<p>Puss i ljumsken ❤️</p>`;
+
 module.exports = ({ env }) => ({
   url: '/dashboard',
   auth: {
     secret: env('ADMIN_JWT_SECRET', '9f1c6109a159dca881e139dc6edf1393'),
+  },
+  forgotPassword: {
+    from: 'no-reply@esek.se',
+    replyTo: 'macapar+strapi@esek.se',
+    template: {
+      subject: 'Glömt ditt lösenord?',
+      text: emailTemplate,
+      html: emailTemplate,
+    },
   },
 });
