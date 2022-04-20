56d4501fb7087a2ddf026371377ddf802c862639
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2022-04-20 12:38:05 +0200

    add url

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('STRAPI_HOST', '0.0.0.0'),
   port: env.int('STRAPI_PORT', 1337),
+  url: env('STRAPI_URL', 'http://localhost:1337'),
   app: {
     keys: env.array('STRAPI_APP_KEYS'),
   },
