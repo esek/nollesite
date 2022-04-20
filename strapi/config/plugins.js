8f9144c1dc1a125faacfaad3b9b1782a85ef6803
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2022-04-20 12:23:04 +0200

    fix email?

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -3,4 +3,13 @@ module.exports = {
     enabled: true,
     resolve: './src/plugins/previewer',
   },
+  email: {
+    config: {
+      provider: 'sendmail',
+      settings: {
+        defaultFrom: 'noreply@esek.se',
+        defaultReplyTo: 'macapar+strapi@esek.se',
+      },
+    },
+  },
 };
