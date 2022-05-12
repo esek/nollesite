44d02bb5739d333fa44392ac2bf51d4c7d089341
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2022-05-12 17:50:15 +0200

    add year gradient view

--- web/src/components/layout/hero/hero.tsx
+++ web/src/components/layout/hero/hero.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { FiChevronDown } from 'react-icons/fi';
+import styles from '@/styles/hero.module.scss';
 
 type Props = {
   title: string;
+  year: string;
 };
 
-const Hero: React.FC<Props> = ({ title }) => {
+const Hero: React.FC<Props> = ({ title, year }) => {
   const scrollToFirst = () => {
     const section = document.querySelector('.strapi-component');
     if (!section?.id) {
@@ -24,14 +26,18 @@ const Hero: React.FC<Props> = ({ title }) => {
   return (
     <section
       data-component="hero"
-      className="relative mt-24 flex h-[80vh] flex-col items-center justify-center"
+      className="relative flex h-[90vh] flex-col items-center justify-center pt-[45vh]"
     >
-      <h1 className="max-w-[30ch] self-center px-8 pb-8 text-center font-nolle text-5xl md:px-0 xl:text-7xl">
+      <h1 className="max-w-[30ch] self-center px-8 text-center font-nolle text-5xl md:px-0 xl:text-7xl">
         {title}
       </h1>
 
+      <div className={styles.herobanner}>
+        <span>{year}</span>
+      </div>
+
       <button
-        className="animate-bounce text-3xl text-accent"
+        className="mt-auto mb-24 animate-bounce text-3xl text-accent"
         onClick={scrollToFirst}
         aria-label="Scroll to content"
       >
