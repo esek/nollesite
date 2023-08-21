3dbfd40db0b3b721977a61f29e9916f747396a6b
Eric Weidow <85724887+Studsministern@users.noreply.github.com>
2023-08-21 23:06:24 +0200

    follow correct project methodology

--- web/src/components/common/components/calendar/calendar-day.tsx
+++ web/src/components/common/components/calendar/calendar-day.tsx
@@ -1,16 +1,15 @@
-import { CalendarEventsGroupedByDay } from '@/models/calendar';
+import { CalendarEvent, CalendarEventsGroupedByDay } from '@/models/calendar';
 import dayjs from 'dayjs';
 import React from 'react';
-import CalendarEvent from './calendar-event';
+import CalendarEventComponent from './calendar-event';
 
 const CalendarDay: React.FC<CalendarEventsGroupedByDay> = ({
   date,
   events,
 }) => {
-  // Sorting events based on the start time
-  events.sort((a, b) =>
-    a.start > b.start ? 1 : -1
-  )
+  // Sorting events by their start time
+  const sortEvents = (e: CalendarEvent[]) =>
+    e.sort((a, b) => (a.start > b.start ? 1 : -1));
 
   return (
     <div className="my-4 space-y-4">
@@ -19,8 +18,8 @@ const CalendarDay: React.FC<CalendarEventsGroupedByDay> = ({
       </h3>
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        {events.map((e) => (
-          <CalendarEvent {...e} key={`calendar-event-${e.id}`} />
+        {sortEvents(events).map((e) => (
+          <CalendarEventComponent {...e} key={`calendar-event-${e.id}`} />
         ))}
       </div>
     </div>
