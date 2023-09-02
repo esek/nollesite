c17eefadbbf06e17a66bd698e0e393a0af09d51c
Eric Weidow <85724887+Studsministern@users.noreply.github.com>
2023-09-02 13:17:37 +0200

    move calendar day sorting to api route

--- web/src/components/common/components/calendar/calendar-day.tsx
+++ web/src/components/common/components/calendar/calendar-day.tsx
@@ -7,10 +7,6 @@ const CalendarDay: React.FC<CalendarEventsGroupedByDay> = ({
   date,
   events,
 }) => {
-  // Sorting events by their start time
-  const sortEvents = (e: CalendarEvent[]) =>
-    e.sort((a, b) => (a.start > b.start ? 1 : -1));
-
   return (
     <div className="my-4 space-y-4">
       <h3 className="text-lg font-semibold capitalize">
@@ -18,7 +14,7 @@ const CalendarDay: React.FC<CalendarEventsGroupedByDay> = ({
       </h3>
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        {sortEvents(events).map((e) => (
+        {events.map((e) => (
           <CalendarEventComponent {...e} key={`calendar-event-${e.id}`} />
         ))}
       </div>
